8cbce9e61f82d6e31fbba69d723f15744c1f387f
Dob <lalilulelo.mg@gmail.com>
2023-07-07 21:10:50 +1000

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+   <div className="flex justify-center items-center h-full" >
+    <Button size="lg" variant={"destructive"} >Click me</Button>
+   </div>
+  )
+}
