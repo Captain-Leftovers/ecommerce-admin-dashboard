cd79fb3ed705169424035203b1e168fe052a8ced
Dob <lalilulelo.mg@gmail.com>
2023-07-15 21:22:52 +1000

    added category api routes

---
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -0,0 +1,125 @@
+import prismaDB from '@/lib/prismaDB'
+import { auth } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
+
+export async function PATCH(
+	req: Request,
+	{ params }: { params: { storeId: string; categoryId: string } }
+) {
+	try {
+		const { userId } = auth()
+		const body = await req.json()
+		const { name, billboardId } = body
+
+		if (!userId) {
+			return new NextResponse('Unauthorized', { status: 401 })
+		}
+
+		if (!name) {
+			return new NextResponse('Missing Name', { status: 400 })
+		}
+
+		if (!billboardId) {
+			return new NextResponse('Missing Billboard Id', { status: 400 })
+		}
+
+		if (!params.categoryId) {
+			return new NextResponse('Missing Category Id', { status: 400 })
+		}
+
+		const storeByUserId = await prismaDB.store.findFirst({
+			where: {
+				id: params.storeId,
+				userId: userId,
+			},
+		})
+
+		if (!storeByUserId) {
+			return new NextResponse('Unauthorized', { status: 403 })
+		}
+
+		const category = await prismaDB.category.updateMany({
+			where: {
+				id: params.categoryId,
+			},
+			data: {
+				name: name,
+				billboardId: billboardId,
+			},
+		})
+
+		return NextResponse.json(category)
+	} catch (error) {
+		console.log('[CATEGORY_PATCH_ERROR]', error)
+		return new NextResponse('Internal error', { status: 500 })
+	}
+}
+
+export async function DELETE(
+	req: Request,
+	{
+		params,
+	}: {
+		params: { storeId: string; categoryId: string }
+	}
+) {
+	try {
+		const { userId } = auth()
+
+		if (!userId) {
+			return new NextResponse('Unauthorized', { status: 401 })
+		}
+
+		if (!params.categoryId) {
+			return new NextResponse('Missing Category Id', { status: 400 })
+		}
+
+		const storeByUserId = await prismaDB.store.findFirst({
+			where: {
+				id: params.storeId,
+				userId: userId,
+			},
+		})
+
+		if (!storeByUserId) {
+			return new NextResponse('Unauthorized', { status: 403 })
+		}
+
+		const category = await prismaDB.category.deleteMany({
+			where: {
+				id: params.categoryId,
+			},
+		})
+
+		return NextResponse.json(category)
+	} catch (error) {
+		console.log('[CATEGORY_DELETE_ERROR]', error)
+		return new NextResponse('Internal error', { status: 500 })
+	}
+}
+
+export async function GET(
+	req: Request,
+	{
+		params,
+	}: {
+		params: { categoryId: string }
+	}
+) {
+	try {
+		if (!params.categoryId) {
+			return new NextResponse('Missing Category Id', { status: 400 })
+		}
+
+		const category = await prismaDB.category.findUnique({
+			where: {
+				id: params.categoryId,
+			},
+		})
+
+		return NextResponse.json(category)
+	} catch (error) {
+		console.log('[CATEGORY_GET_ERROR]', error)
+		return new NextResponse('Internal error', { status: 500 })
+	}
+}
---
+++ app/api/[storeId]/categories/route.ts
@@ -0,0 +1,78 @@
+import prismaDB from '@/lib/prismaDB'
+import { auth } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
+
+export async function POST(
+	req: Request,
+	{ params }: { params: { storeId: string } }
+) {
+	try {
+		const { userId } = auth()
+		const body = await req.json()
+
+		if (!userId) {
+			return new NextResponse('Unauthenticated', { status: 401 })
+		}
+
+		const { name, billboardId } = body
+
+		if (!name) {
+			return new NextResponse('Name is required', { status: 400 })
+		}
+		if (!billboardId) {
+			return new NextResponse('Billboard Id is required', { status: 400 })
+		}
+
+		if (!params.storeId) {
+			return new NextResponse('StoreId is required', { status: 400 })
+		}
+
+		const storeByUserId = await prismaDB.store.findFirst({
+			where: {
+				id: params.storeId,
+				userId: userId,
+			},
+		})
+
+		if (!storeByUserId) {
+			return new NextResponse('Unauthorized', { status: 403 })
+		}
+
+		const category = await prismaDB.category.create({
+			data: {
+				billboardId: billboardId,
+				name: name,
+				storeId: params.storeId,
+			},
+		})
+
+		return NextResponse.json(category)
+	} catch (error) {
+		console.log('[CATEGORIES_POST]', error)
+		return new NextResponse('Internal Server Error', { status: 500 })
+	}
+}
+
+export async function GET(
+	req: Request,
+	{ params }: { params: { storeId: string } }
+) {
+	try {
+		if (!params.storeId) {
+			return new NextResponse('StoreId is required', { status: 400 })
+		}
+
+		const categories = await prismaDB.category.findMany({
+			where: {
+				storeId: params.storeId,
+			},
+		})
+
+		return NextResponse.json(categories)
+	} catch (error) {
+		console.log('[CATEGORIES_GET]', error)
+		return new NextResponse('Internal Server Error', { status: 500 })
+	}
+}
+
+//TODO : remember if you have error somewhere that params are the second argument of the function thats why we need re:Request as the first argument
