e96765b92f6da7ad66c2fbb260d4f95ddd249291
beeondweb <beeondweb@gmail.com>
2024-03-30 17:16:43 +1100

    turso-1

--- lib/prismaDB.ts
+++ lib/prismaDB.ts
@@ -1,12 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
-declare global {
-    var prisma: PrismaClient | undefined;
-}
+const libsql = createClient({
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN,
+});
 
-const prismaDB = globalThis.prisma || new PrismaClient();
+const adapter = new PrismaLibSQL(libsql);
+const prismaDB = new PrismaClient({ adapter });
 
-if (process.env.NODE_ENV !== "production" ) globalThis.prisma = prismaDB;
 
-
-export default prismaDB;
+export default prismaDB
